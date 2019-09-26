04a1de7f659492307733269723508fbb2cc54c58
Markosaur <mark3pace@gmail.com>
2019-09-26 10:59:45 -0600

    routing

--- __instructor-code__/src/App.js
+++ __instructor-code__/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Header from './components/Header/Header'
 import routes from './routes'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       {routes}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
